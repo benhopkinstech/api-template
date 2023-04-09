e2b1616e102a08a20b1fc9cfda8310fef3009cb3
Ben Hopkins <benhopkinstech@gmail.com>
2023-04-09 13:20:00 +0100

    Added some example tests including authorization

---
+++ Api.Tests/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using Api.Modules.Identity.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Api.Tests
+{
+    public static class Extensions
+    {
+        public static async Task<HttpResponseMessage> RegisterWithCredentialsAsync(HttpClient client, string email, string password)
+        {
+            var credentials = new CredentialsModel()
+            {
+                Email = email,
+                Password = password
+            };
+
+            var response = await client.PostAsJsonAsync<CredentialsModel>("identity/register", credentials);
+
+            return response;
+        }
+
+        public static async Task<HttpResponseMessage> LoginWithCredentialsAsync(HttpClient client, string email, string password)
+        {
+            var credentials = new CredentialsModel()
+            {
+                Email = email,
+                Password = password
+            };
+
+            var response = await client.PostAsJsonAsync<CredentialsModel>("identity/login", credentials);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return response;
+        }
+    }
+}
--- Api.Tests/IdentityModuleTests.cs
+++ Api.Tests/IdentityModuleTests.cs
@@ -14,16 +14,54 @@ namespace Api.Tests
         {
             await ResetDatabse();
 
-            var credentials = new CredentialsModel();
-            var response = await _client.PostAsJsonAsync<CredentialsModel>("identity/register", credentials);
+            var response = await Extensions.RegisterWithCredentialsAsync(_client, "", "");
 
-            Assert.True(response.StatusCode == System.Net.HttpStatusCode.BadRequest);
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
 
-            credentials.Email = "test@test.com";
-            credentials.Password = "password";
-            response = await _client.PostAsJsonAsync<CredentialsModel>("identity/register", credentials);
+            response = await Extensions.RegisterWithCredentialsAsync(_client, "test@test.com", "password");
 
-            Assert.True(response.StatusCode == System.Net.HttpStatusCode.Created);
+            Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestPostRegisterExists()
+        {
+            var response = await Extensions.RegisterWithCredentialsAsync(_client, "test@test.com", "password");
+
+            Assert.Equal(System.Net.HttpStatusCode.Conflict, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestPostLogin()
+        {
+            var response = await Extensions.LoginWithCredentialsAsync(_client, "", "");
+
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+
+            response = await Extensions.LoginWithCredentialsAsync(_client, "test@test.com", "password");
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestPutPassword()
+        {
+            var passwordUpdate = new PasswordUpdateModel();
+            var response = await _client.PutAsJsonAsync<PasswordUpdateModel>("identity/password", passwordUpdate);
+
+            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+
+            await Extensions.LoginWithCredentialsAsync(_client, "test@test.com", "password");
+
+            response = await _client.PutAsJsonAsync<PasswordUpdateModel>("identity/password", passwordUpdate);
+
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+
+            passwordUpdate.Password = "P4ssW0rd!";
+            passwordUpdate.CurrentPassword = "password";
+            response = await _client.PutAsJsonAsync<PasswordUpdateModel>("identity/password", passwordUpdate);
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
