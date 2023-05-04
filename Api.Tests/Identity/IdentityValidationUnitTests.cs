3475b2fc805cf27a376829025a707a6eb8b1b084
Ben Hopkins <benhopkinstech@gmail.com>
2023-05-04 21:23:01 +0100

    Added validation unit tests

---
+++ Api.Tests/Identity/IdentityValidationUnitTests.cs
@@ -0,0 +1,56 @@
+﻿using Api.Modules.Identity.Interfaces;
+using Api.Modules.Identity.Services;
+using Xunit;
+
+namespace Api.Tests.Identity
+{
+    public class IdentityValidationUnitTests
+    {
+        private readonly IValidationService _validation;
+
+        public IdentityValidationUnitTests()
+        {
+            _validation = new ValidationService();
+        }
+
+        [Fact]
+        public void SevenCharacterPassword_HasErrors()
+        {
+            const string password = "aaaaaaa";
+
+            var errors = _validation.PasswordCheck(password);
+
+            Assert.NotEmpty(errors);
+        }
+
+        [Fact]
+        public void SeventyThreeCharacterPassword_HasErrors()
+        {
+            const string password = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+            var errors = _validation.PasswordCheck(password);
+
+            Assert.NotEmpty(errors);
+        }
+
+        [Fact]
+        public void TwoHundredFiftySevenCharcterEmail_HasErrors()
+        {
+            const string email = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@email.com";
+
+            var errors = _validation.EmailCheck(email);
+
+            Assert.NotEmpty(errors);
+        }
+
+        [Fact]
+        public void WithoutAtEmail_HasErrors()
+        {
+            const string email = "testemail.com";
+
+            var errors = _validation.EmailCheck(email);
+
+            Assert.NotEmpty(errors);
+        }
+    }
+}
